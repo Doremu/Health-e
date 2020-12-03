e38a27d5a821263fe6c9d85cb5a8f3b3e18ecd6a
Michael <michaelfrans.53@gmail.com>
2020-12-03 08:16:16 +0700

    logout di dokter

--- lib/dokter/mainDokter.dart
+++ lib/dokter/mainDokter.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healthe/constants.dart';
 
 class mainDokter extends StatefulWidget {
   @override
@@ -25,13 +27,37 @@ class _HomeDokterState extends State<mainDokter> {
                 children: <Widget>[
                   // _temperature(),
                   // _pasien(),
-                  // _content()
+                  // _content(),
+                  _logout()
                 ],
               ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _logout() {
+    return InkWell(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        width: double.infinity,
+        child: Text(
+          'Logout',
+          style: TextStyle(color: ColorPalette.primaryColor),
+          textAlign: TextAlign.center,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+      onTap: () async {
+        // Navigator.pop(context);
+        await FirebaseAuth.instance.signOut();
+        Navigator.pushNamed(context, "/login");
+      },
     );
   }
   
